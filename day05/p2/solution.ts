fdc1fe5d971d53a39babeec2e9ddaff2001f4ce3
James G. E. McIlhargey <jmcilhargey@bloomberg.net>
2022-12-05 07:16:23 -0500

    Day 5

---
+++ day05/p2/solution.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs';
+
+fs.readFile("/dev/AOC_22/day05/p1/input.data", 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    const inputData = data.split('\n');
+    let splitIndex = inputData.indexOf("");
+    let crateData = inputData.slice(0,splitIndex).map(row => row.split(''));
+    let moves = inputData.slice(splitIndex+1);
+
+    let stacks = {};
+    let numstacks = 0;
+    let l = crateData.length-1;
+    for (let i = 0; i < crateData[l].length; i++) {
+        let char = crateData[l][i];
+        if (char !== " ") {
+            let newStack = [];
+            let j = l-1;
+            while (j >= 0 && crateData[j][i] !== " ") {
+                newStack.push(crateData[j][i]);
+                j--;
+            }
+            stacks[char] = newStack;
+            numstacks++;
+        }
+    }
+
+    for (let move of moves) {
+        let movesplit = move.split(" ");
+        let count = parseInt(movesplit[1],10);
+        let from = movesplit[3];
+        let to = movesplit[5];
+
+        stacks[to] = stacks[to].concat(stacks[from].slice(-1*count));
+        stacks[from] = stacks[from].slice(0,stacks[from].length-count);
+    }
+
+
+    let topcrates = "";
+    for (let i = 1; i < numstacks+1; i++) {
+        topcrates = topcrates + (stacks[i.toString()].pop() || " ");
+    }
+    console.log(`Top crates: ${topcrates}`)
+});
