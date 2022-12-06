0e8a8f24869839780b88238f6ac27640a45d63dc
James G. E. McIlhargey <jmcilhargey@bloomberg.net>
2022-12-06 09:13:42 -0500

    Day 6

---
+++ day06/p2/solution.ts
@@ -0,0 +1,20 @@
+import * as fs from 'fs';
+
+fs.readFile("/dev/AOC_22/day06/p2/input.data", 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    let position = 0;
+    const numDistinct = 14;
+    while (position < data.length - numDistinct) {
+        let substr = data.slice(position, position+numDistinct);
+        let charset = new Set(substr.split(""));
+        if (charset.size === numDistinct) {
+            break;
+        }
+        position++;
+    }
+    console.log(`Start marker is at ${position + numDistinct}`)
+});
